d3d318b6eaba74cdf6fc67d24bff77cc3b2cec1c
Deanna Vickers <deanna2000@gmail.com>
2018-08-27 15:57:10 -0500

    show birthdays on DOM

--- brjr/static/js/main.js
+++ brjr/static/js/main.js
@@ -1,12 +1,16 @@
 document.getElementById("js-clicker").addEventListener("click", () =>  {
+	document.getElementById("js-clicker").style.visibility = 'hidden'
 	fetch('/birthdays')
 	.then(response => {
 		return response.json();
 	})
 	.then(birthdays => {
-		console.log(JSON.stringify(birthdays));
-	});
+		let listEl = document.getElementById("birthdayList")   // Target the location
+  		for(i=0; i<birthdays.length; i++) {
+        	let liElement = document.createElement("li")    // Create the li element
+        	let textNode = document.createTextNode(birthdays[i].name + "       " + birthdays[i].date + "     " + birthdays[i].greeting)   // Create text snippet we are going to use
+        	liElement.appendChild(textNode)    // Add the text snippet to the li element
+        	listEl.appendChild(liElement)    // Add the li element+text to the html document location (ul in this case)
+}}
+)
 })
-
-
-
